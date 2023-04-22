190bbeb02d8494a82301edb2497fd0a009089ea8
Iyad Ahmed <iyadahmed430@gmail.com>
2023-04-22 11:06:22 +0200

    Update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 from math import floor
 
 WINDOW_NAME = "Camera"
-STEP_SIZE = 30  # The height of each row in pixels
+STEP_SIZE = 10  # The height of each row in pixels
 
 
 def main():
@@ -21,24 +21,14 @@ def main():
         if not success:
             break
 
+        # Convert to grayscale
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        frame = cv2.Canny(frame, 100, 1000, apertureSize=7, L2gradient=False)
-
-        # Denoise https://docs.opencv.org/4.7.0/d5/d69/tutorial_py_non_local_means.html
-        # frame = cv2.fastNlMeansDenoising(frame)
-
-        frame = frame.astype("f")
-
-        # frame = cv2.blur(frame, (10, 10))
-        # frame = cv2.GaussianBlur(frame, (15, 15), 0)
-        frame *= 0.01
+        # Apply color map
+        frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
 
         # Discretize image
-        # frame = frame[0::STEP_SIZE, :].repeat(STEP_SIZE, axis=0)
-
-        # Apply color map
-        # frame = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
+        frame = frame[0::STEP_SIZE, :].repeat(STEP_SIZE, axis=0)
 
         # Show image
         cv2.imshow(WINDOW_NAME, frame)
